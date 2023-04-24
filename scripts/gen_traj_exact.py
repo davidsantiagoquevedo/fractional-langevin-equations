bd5302189f7f8b51eafdaa847bc75a2031d405ac
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2023-04-24 18:35:37 +0200

    feat: added scripts to run and generate trajectories

---
+++ scripts/gen_traj_exact.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 7 2022
+@author: davidsantiagoquevedo
+"""
+import sys
+h = float(sys.argv[1])
+n = int(sys.argv[2])
+T = int(sys.argv[3])
+trajectories = int(sys.argv[4])
+
+DATA_PATH = "data/"
+
+sys.path.append("src/")
+import fBm_exact as fbml
+import utils as ut
+
+df_fbm, df_fgn = fbml.frac_brown_davies_harte(h, n, T, trajectories)
+
+ut.write_hdf5(DATA_PATH + f"fBm_dh-h-{h}-{trajectories}.hdf5", df_fbm)
+ut.write_hdf5(DATA_PATH + f"fgn_dh-h-{h}-{trajectories}.hdf5", df_fbm)
---
+++ scripts/gen_traj_integration.jl
@@ -0,0 +1,24 @@
+#= Created on Tue Dec 7 2022
+@author: davidsantiagoquevedo
+Adapted from: https://github.com/17thSaint/finance-thesis
+=#
+
+using HDF5
+
+DATA_PATH = "data/"
+
+include("../src/utils.jl")
+include("../src/fBm_integration.jl")
+using .utils, .fbm_integration
+
+h = parse(Float64,ARGS[1])
+n = parse(Int64,ARGS[2])
+T = parse(Int64,ARGS[3])
+trajectory_i = parse(Int64,ARGS[4])
+trajectory_f = parse(Int64,ARGS[5])
+
+for j in trajectory_i:trajectory_f
+    path = make_path(h, j, DATA_PATH)
+	trajectory = frac_brown_wiki2(h,n,T)
+	write_data_hdf5(path,trajectory)
+end
