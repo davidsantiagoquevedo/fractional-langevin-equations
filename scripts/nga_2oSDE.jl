4313ae51fac55711e650ce7f8ec1a900583bbe5e
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2023-05-10 19:06:33 +0200

    fixed: problems with initial conditions for 2oSDE. Added solutions with zero and non-zero conditions.

--- scripts/nga_2oSDE.jl
+++ scripts/nga_2oSDE.jl
@@ -11,27 +11,34 @@ using .utils, .fbm_integration, PyPlot
 pygui(true)
 
 
-function get_noise(h, n, t_fin, which = rand(1:10), dir ="", make_new = false)
-	fBM = read_hdf5_data(h, which, dir, true, n+1)[2]
+function get_noise(h, N, t_fin, which = rand(1:10), dir ="", make_new = false)
+	fBM = read_hdf5_data(h, which, dir, true, N+1)[2]
 	if make_new
-		fBM = frac_brown_wiki2(h, n, t_fin)[2]
+		fBM = frac_brown_wiki2(h, N, t_fin)[2]
 	end
-	return [t_fin*(fBM[i+1] - fBM[i])/n for i in 1:n]
+
+	return [t_fin*(fBM[i+1] - fBM[i])/N for i in 1:N]
 end
 
 """
-    get_first_guess(time_told)
+    get_first_guess(N, delta_t, x0, v0)
 
 First guess of the Monte Carlo Algorithm. 
 For the Fractional Langevin equations, the first guess is the analytical solution of the integer order part of de differential equation.
-However, for this first benchmark we use x = 0
+However, for this benchmark we use x = [x0, x1, 0, 0, 0, ...]
 
 ARGS
-    time_told (int): number of points in time to guess
+    N (int): length of the configuration - number of points in the trajectory
+	delta_t (float): time delta between points
+	x0 (float): initial position of the system
+	v0 (float): initial velocity of the system
 """
-function get_first_guess(time_told)
-	x0 = [0.0 for _ in 1:time_told]
-    return x0
+function get_first_guess(N, delta_t, x0, v0)
+	x1 = x0 + v0*delta_t
+	x_guess = [0.0 for _ in 3:N]
+	x_guess = append!([x0, x1], x_guess)
+
+    return x_guess
 end
 
 """
@@ -40,191 +47,174 @@ Integer order part of the differential equation
 Removed get_scale_inv_vals: Notice that in the original implementation they were returning 1.
 
 ARGS
-	config (array): current state/configuration of the solution. For the langevin equation is the position
-	delta_t (float): delta time to calculate de derivatives
+	config (array): current state/configuration of the system. For the langevin equation is the position
+	delta_t (float): delta time between tow points on the configuration (N/t_fin)
 	noise (array): noise term of the equation
-	noise_steps (int). Defaults to 1: number of noise points asociated to every point in time of the configuration
 """
-function get_goft(config, delta_t, noise, noise_steps = 1)
-	sliced_noise = [noise[i*noise_steps + 1] for i in 1:length(config) -2]
+function get_goft(config, delta_t, noise)
+	sliced_noise = [noise[i] for i in 1:length(config)-2]
 	velocity = [(config[i+1] - config[i])/delta_t for i in 1:length(config)-1]
 	accel = [(velocity[i+1] - velocity[i])/delta_t for i in 1:length(velocity)-1]
 	g_of_t = accel - sliced_noise
+
 	return g_of_t, velocity, accel
 end
 
 """
-	get_resids(config,delta_t,noise, noise_steps)
+	get_resids(config, delta_t, noise)
 Get the residuals between the left-hand side and the right-hand side of the SDE.
 For the fractional Langevin equation it's the residual between the analytically tratable part and the fractional derivative.
 ARGS
-	config (array): current state/configuration of the solution. For the langevin equation is the position
-	delta_t (float): delta time to calculate de derivatives
+	config (array): current state/configuration of the system. For the langevin equation is the position
+	delta_t (float): delta time between tow points on the configuration (N/t_fin)
 	noise (array): noise term of the equation
-	noise_steps (int). Defaults to 1: number of noise points asociated to every point in time of the configuration
 """
-function get_resids(config,delta_t,noise, noise_steps = 1)
-	g_stuff = get_goft(config, delta_t, noise, noise_steps)
-	steps = length(config)
-	right_hand_side = [0 for _ in 2:steps-1]
+function get_resids(config, delta_t, noise)
+	g_stuff = get_goft(config, delta_t, noise)
+	right_hand_side = [0.0 for _ in 1:length(g_stuff[1])]
+
 	return abs.(g_stuff[1] - right_hand_side)
 end
 
 """
-	move_position(num_times, chosen, step_size)
+	move_position(N, chosen, step_size)
 Suggest a shift matrix to change the configuration at a certain point in time using a random number with a maximum step size
 ARGS
-	config (array): current state/configuration of the solution. For the langevin equation is the position
-	delta_t (float): delta time to calculate de derivatives
-	noise (array): noise term of the equation
-	noise_steps (int). Defaults to 1: number of noise points asociated to every point in time of the configuration
+	N (int): length of the configuration - number of points in the trajectory
+	chosen (int): point in the configuration to shift
+	step_size (array): maximum value to shift
 """
-function move_position(num_times, chosen, step_size)
-	shift_matrix = [0.0 for _ = 1:num_times]
+function move_position(N, chosen, step_size)
+	shift_matrix = [0.0 for _ in 1:N]
 	shift_matrix[chosen] += rand(-1:2:1)*rand(Float64)*step_size
 	return shift_matrix
 end
 
 """
-	move_position(num_times, chosen, step_size)
-Get the residuals between the left-hand side and the right-hand side of the SDE.
-For the fractional Langevin equation it's the residual between the analytically tratable part and the fractional derivative.
+	acc_rej_move(config, N, chosen, step_size, delta_t, noise, metro_tol)
+Accept or reject new configuration based on tolerance value (metro_tol)
 ARGS
-	num_times (array): lenght of the configuration (solution)
-	chosen (int): point in the solution to shift
+	N (int): length of the configuration - number of points in the trajectory
+	chosen (int): point in the configuration to shift
 	step_size (array): maximum value to shift
-	delta_t (float): delta time to calculate de derivatives
+	delta_t (float): delta time between tow points on the configuration (N/t_fin)
 	noise (array): noise term of the equation
-	noise_steps (int). Defaults to 1: number of noise points asociated to every point in time of the configuration
-	top_val (float): Maximum value accepted to accept or reject a shift
+	metro_tol (float): maximum tolerance value to accept or reject a shift
 """
-function acc_rej_move(config, num_times, chosen, step_size, delta_t, noise, noise_steps, top_val)
-	start_resids = get_resids(config, delta_t, noise, noise_steps)
-	
-    shift_matrix = move_position(num_times, chosen, step_size)
-	new_resids = get_resids(config + shift_matrix, delta_t, noise, noise_steps)
-	
+function acc_rej_move(config, N, chosen, step_size, delta_t, noise, metro_tol)
+	start_resids = get_resids(config, delta_t, noise)
+    shift_matrix = move_position(N, chosen, step_size)
+	new_resids = get_resids(config + shift_matrix, delta_t, noise)
     exp_diff = exp.(new_resids - start_resids)
-    checking = exp_diff[chosen-2] <= top_val
 	
+    checking = exp_diff[chosen-2] <= metro_tol # The acceptance rule must be evaluated in the position i-2 
+												# because we are adjusting the second order derivative
     if checking
-		return config + shift_matrix, 1#, new_resids, start_resids, shift_matrix
+		return config + shift_matrix, 1
 	else
-		return config, 0 #, new_resids, start_resids, shift_matrix
+		return config, 0
 	end
 end
 
 """
-	main_here(tol,steps,step_size, time_told, t_fin, noise, noise_steps, top_val, fixed)
-Run main Monte Carlo loop of the Next guess Algorithm
+	main_here(mc_steps, step_size, N, t_fin, noise, error_tol, metro_tol, x0, v0)
+Main loop for the Monte Carlo of the Next Guess Algorithm
 ARGS
-	tol (float): tolerance. Maximum value accepted for the residuals
-	steps (int): Monte Carlo steps to run over
+	
+	mc_steps (int): Monte Carlo iterations
 	step_size (array): maximum value to shift when moving
 	time_told (int): lenght of the array containing the solution
 	t_fin (int): lenght of the time interval to solve. Notice that delta_t = time_told/t_fin
 	noise (array): noise term of the equation
-	noise_steps (int). Defaults to 1: number of noise points asociated to every point in time of the configuration
-	top_val (float): Maximum value accepted to accept or reject a shift
-	fixed (float): Fixed initial condition
+	error_tol (float): maximum value accepted for the residuals of the whole system
+	metro_tol (float): maximum tolerance value to accept or reject a shift
+	x0 (float): initial position of the system
+	v0 (float): initial velocity of the system
 """
-function main_here(tol,steps,step_size, time_told, t_fin, noise, noise_steps, top_val, fixed)
-	# getting first config from first guess
-	running_config = append!([0.0,fixed],get_first_guess(time_told)[3:time_told])
+function main_here(mc_steps, step_size, N, t_fin, noise, error_tol, metro_tol, x0, v0, sampling = false)
+	delta_t = t_fin/N
+	running_config = get_first_guess(N, delta_t, x0, v0)
 	
-    # only save configuration data for every 10 attempted movements
-	samp_freq = 10
+	# Save configuration at every 10 attempted moves
+	samp_freq = 10 
+	samp_index = 1
+	if sampling
+		time_config = fill(0.0, (N, Int(mc_steps/samp_freq)))
+		time_resids = fill(0.0, (N-2, Int(mc_steps/samp_freq)))
+	else
+		time_config = false
+		time_resids = false
+	end
 
-	time_config = fill(0.0,(time_told,Int(steps/samp_freq)))
-	time_resids = fill(0.0,(time_told-2,Int(steps/samp_freq)))
+	# Beginning of Monte Carlo loop
+	num_wrong = [i for i in 3:N] # Assumed initial wrong points - all except for x0 and x1
+	acc_rate = 0 # Acceptance rate of the Monte Carlo
+	trial_index = 0
 
-	index = 1
-	index2 = 0
-
-	delta_t = t_fin/time_told
-	acc_rate = 0
-
-	num_wrong = [i+2 for i in 1:time_told-2]
-	
-	
-	for i in 1:steps
-		# each MC time step every time point has attempted move, except starting point
-		#=
-		for k in 3:time_told
-			movement = acc_rej_move(running_config,h,time_told,k,step_size,delta_t,noise,noise_steps,top_val)
-			running_config = movement[1]
-			acc_rate += movement[2]
-			#println(movement[2],", ",movement[3],movement[4],movement[5])
-		end
-		=#
-		
+	for i in 1:mc_steps
 		upper = 4
 		if length(num_wrong) < 4
 			upper = length(num_wrong)
 		end
 		
-		for k in num_wrong[1:upper]#3 + num_correct:upper
-			movement = acc_rej_move(running_config, time_told, k, step_size, delta_t, noise, 1, top_val)
+		for k in num_wrong[1:upper]
+			movement = acc_rej_move(running_config, N, k, step_size, delta_t, noise, metro_tol)
 			running_config = movement[1]
 			acc_rate += movement[2]
-			index2 += 1
+			trial_index += 1
 		end
+			
+		# Update wrong points
 		num_wrong = []
-		
-		current_res = get_resids(running_config, delta_t, noise, noise_steps)
-		
-		# saving data every 10 steps
-		if i%samp_freq == 0
-			time_config[:,index] = [running_config[x] for x in 1:time_told]
-			time_resids[:,index] = [current_res[y] for y in 1:time_told-2]
-			index += 1
-		end
-		
-		# if every time point has residuals less than tolerance then solution is found
-		check_tol = [ current_res[j] < tol for j in 1:time_told-2 ]
-		for i in 1:time_told-2
-			if check_tol[i]
-				
-			else
-				append!(num_wrong,[i+2])
+		current_res = get_resids(running_config, delta_t, noise)
+		check_tol = [current_res[j] < error_tol for j in 1:N-2]
+		for wr in 1:length(current_res)
+			if ~ check_tol[wr]
+				append!(num_wrong,[wr+2])
 			end
 		end
+
+		# Save data every 10 steps
+		if (i%samp_freq == 0) & (sampling)
+			time_config[:, samp_index] = [running_config[x] for x in 1:N]
+			time_resids[:, samp_index] = [current_res[y] for y in 1:N-2]
+			samp_index += 1
+		end
+
+		# If every time point has residuals less than tolerance then solution is found
 		if all(check_tol)
 			println("Solution Found in $i Steps")
-			return running_config,time_config,time_resids,i
+			return running_config, time_config, time_resids
 		end
 		
 		# interface data
-		if i%(steps*0.01) == 0
-			println("Running:"," ",100*i/steps,"%, ","Acceptance: ",acc_rate,"/",index2,", Number Wrong: ",length(num_wrong))
+		if i%(mc_steps*0.01) == 0
+			println("Running:", " ", 100*i/mc_steps, "%, ", "Acceptance: ", acc_rate, "/", trial_index, ", Number Wrong: ", length(num_wrong))
 		end
 	end
 	
 	println("No Solution")
-	return time_config, time_resids
+	return running_config, time_config, time_resids
 end
 
-
+######################### ######################### #########################
+#########################       RUN ALGORITHM       #########################
+######################### ######################### #########################
 h = 0.5
-
 final_time = 30
-time_steps = 30
+N = 500
 noise_steps = 1
 
-noise = get_noise(h, noise_steps*time_steps, final_time, 1, DATA_PATH)
-plot(noise)
+noise = get_noise(h, noise_steps*N, final_time, 1, DATA_PATH)
 
-times = [i*final_time/time_steps for i in 0:time_steps-1]
-
-
-tol = 0.0001
-mc_steps = 1000000
-metro_val = 1.000001
+error_tol = 0.0001
+mc_steps = 111000000
+metro_tol = 1.000001
 step_size = 0.001#0.008*final_time/time_steps
+x0 = 0
+v0 = 3
+times = [i*final_time/N for i in 0:N-1]
 
-
-num_soln = main_here(tol, mc_steps, step_size, time_steps, final_time, noise, noise_steps, metro_val, 0)
-
-plot(num_soln[1])
-
-num_soln[]
+num_soln = main_here(mc_steps, step_size, N, final_time, noise, error_tol, metro_tol, x0, v0)
+write_data_hdf5(DATA_PATH * "2o_sde-h-0.5-noise$N.hdf5", (times, noise))
+write_data_hdf5(DATA_PATH * "2o_sde-h-0.5-$N-v0$v0.hdf5", (times, num_soln[1]))
