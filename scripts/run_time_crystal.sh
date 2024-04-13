a4a9af89ed7d6a17db617173defd61790aa06613
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2024-04-13 15:07:40 +0200

    refac: linear 0,1 fix: srun line

--- scripts/run_time_crystal.sh
+++ scripts/run_time_crystal.sh
@@ -19,7 +19,7 @@ conda activate dev_phys
 #TIME GLASS
 TASKNAME="TG"
 
-linear="False"
+linear=0
 M=1
 v0=1
 eta_1=1
@@ -31,13 +31,13 @@ for alpha in 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.3 0.5 0.7 0.9 0.
 do
   out_name=set$TASK_SET-avg$avg-dt$dt-T$T-linear$linear-eta1$eta_1-eta2$eta_2-T1$T1-T2$T2-v0$v0-M$M-alpha$alpha
   JOBNAME=$TASKNAME$alpha
-  srun -n $n_tasks -c $n_cpus_task --time=100:00:00 --job-name=$JOBNAME python scripts/msd_fle_time_crystal_parallel.py $alpha $linear $M $v0 $dt $T $avg $TASK_SET > nohup/$out_name.out 2>&1 &
+  srun -n $n_tasks -c $n_cpus_task --time=100:00:00 --job-name=$JOBNAME python scripts/msd_fle_time_crystal_parallel.py $alpha $linear $M $v0 $eta1 $eta2 $T1 $T2 $dt $T $avg $TASK_SET > nohup/$out_name.out 2>&1 &
 done
 
 #TIME CRYSTAL
 TASKNAME="TC"
 
-linear="False"
+linear=0
 M=1
 v0=1
 eta_1=0
@@ -49,13 +49,13 @@ for alpha in 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.3 0.5 0.7 0.9 0.
 do
   out_name=set$TASK_SET-avg$avg-dt$dt-T$T-linear$linear-eta1$eta_1-eta2$eta_2-T1$T1-T2$T2-v0$v0-M$M-alpha$alpha
   JOBNAME=$TASKNAME$alpha
-  srun -n $n_tasks -c $n_cpus_task --time=100:00:00 --job-name=$JOBNAME python scripts/msd_fle_time_crystal_parallel.py $alpha $linear $M $v0 $dt $T $avg $TASK_SET > nohup/$out_name.out 2>&1 &
+  srun -n $n_tasks -c $n_cpus_task --time=100:00:00 --job-name=$JOBNAME python scripts/msd_fle_time_crystal_parallel.py $alpha $linear $M $v0 $eta1 $eta2 $T1 $T2 $dt $T $avg $TASK_SET > nohup/$out_name.out 2>&1 &
 done
 
 #TIME CRYSTAL + GLASSES
 TASKNAME="TCG"
 
-linear="False"
+linear=0
 M=1
 v0=1
 eta_1=1
@@ -67,13 +67,13 @@ for alpha in 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.3 0.5 0.7 0.9 0.
 do
   out_name=set$TASK_SET-avg$avg-dt$dt-T$T-linear$linear-eta1$eta_1-eta2$eta_2-T1$T1-T2$T2-v0$v0-M$M-alpha$alpha
   JOBNAME=$TASKNAME$alpha
-  srun -n $n_tasks -c $n_cpus_task --time=100:00:00 --job-name=$JOBNAME python scripts/msd_fle_time_crystal_parallel.py $alpha $linear $M $v0 $dt $T $avg $TASK_SET > nohup/$out_name.out 2>&1 &
+  srun -n $n_tasks -c $n_cpus_task --time=100:00:00 --job-name=$JOBNAME python scripts/msd_fle_time_crystal_parallel.py $alpha $linear $M $v0 $eta1 $eta2 $T1 $T2 $dt $T $avg $TASK_SET > nohup/$out_name.out 2>&1 &
 done
 
 #IN EQUILIBRIUM
 TASKNAME="EQ"
 
-linear="True"
+linear=1
 M=1
 v0=1
 eta_1=1
@@ -85,7 +85,7 @@ for alpha in 0.05 0.1 0.3 0.5 0.7 0.9 0.99
 do
   out_name=set$TASK_SET-avg$avg-dt$dt-T$T-linear$linear-eta1$eta_1-eta2$eta_2-T1$T1-T2$T2-v0$v0-M$M-alpha$alpha
   JOBNAME=$TASKNAME$alpha
-  srun -n $n_tasks -c $n_cpus_task --time=100:00:00 --job-name=$JOBNAME python scripts/msd_fle_time_crystal_parallel.py $alpha $linear $M $v0 $dt $T $avg $TASK_SET > nohup/$out_name.out 2>&1 &
+  srun -n $n_tasks -c $n_cpus_task --time=100:00:00 --job-name=$JOBNAME python scripts/msd_fle_time_crystal_parallel.py $alpha $linear $M $v0 $eta1 $eta2 $T1 $T2 $dt $T $avg $TASK_SET > nohup/$out_name.out 2>&1 &
 done
  
 wait
