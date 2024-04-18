b736ae8f236f9b443dddbfa9315f4f9aa8aeb911
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2024-04-18 13:58:25 +0200

    feat: functions to plot py grid

--- src/plot.py
+++ src/plot.py
@@ -7,6 +7,8 @@ from fLe_timecrystal import fle
 import numpy as np
 import pandas as pd
 
+import matplotlib.ticker as tck
+
 def plot_position(eq, 
                   ax,
                   color_fd, color_a = "black",
@@ -56,6 +58,8 @@ def plot_msd(eq, avg, task_set, data_path,
              ax, color_fd, color_a = "black",
              legend_main = False, legend_second = False, **kwargs):
     numeric = numeric_msd(eq, avg, task_set, data_path)
+    if "truncate" in kwargs:
+        numeric = numeric[numeric.t <= kwargs["truncate"]]
     if legend_main:
         numeric.set_index("t").msd.plot(ax = ax, ls = "-", color = color_fd, label = r"$\alpha$ ="+str(eq.alpha))
     else:
@@ -107,3 +111,23 @@ def talpha(t, **kwargs):
     alpha = kwargs["alpha"]
     return t**alpha
 
+def add_freq_grid(ax, freq, T, times = 8):
+    t = 1/freq
+    while t <= T:
+        ax.axvline(x = t, color = "black", alpha = 0.3, ls = ":")
+        t += 1/freq
+        
+    def numfmt(x, pos): # your custom formatter function: divide by 100.0
+        s = '{} $\pi$'.format(int(x / np.pi))
+        return s
+
+    yfmt = tck.FuncFormatter(numfmt)
+    ax.xaxis.set_major_formatter(yfmt)
+    ax.xaxis.set_major_locator(tck.MultipleLocator(base = times*np.pi))
+
+def add_2pi(ax, freq=1/np.pi, T = 7):
+    t = 1/freq
+    while t <= T:
+        ax.axvline(x = t, color = "black", alpha = 0.3, ls = ":")
+        t += 1/freq
+    ax.text(2/freq, .5, "2$\pi$")
