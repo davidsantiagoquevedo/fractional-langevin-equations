dce15b2f0c667d18da5b25c2257abcb64742b44d
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2024-05-08 19:17:08 +0200

    feat: CI 98% fill between plot, fixed initial codition for analytics, change stepsize and T

--- scripts/fig_2_check.py
+++ scripts/fig_2_check.py
@@ -12,27 +12,28 @@ def plot(ax, eq, avg, task_set, data_path, trunc):
     numeric = pt.numeric_msd(eq, avg, task_set, data_path)
     numeric = numeric[numeric.t <= trunc]
     eq_ = fle(eq.alpha, eq.linear)
-    eq_.params(T = trunc, h = eq.h,
-            v0 = 0, M = eq.M,
+    eq_.params(T = trunc, h = 0.1,
+            v0 = eq.v0, M = eq.M,
             eta_1 = eq.eta_1, eta_2 = eq.eta_2,
             T1 = eq.T1, T2 = eq.T2)
     eq_.make_B_H()
     eq_.msd_non_linear()
-    ax.plot(numeric.t, numeric.msd, label = r"$\alpha$ = "+str(eq.alpha))
+    ax.fill_between(numeric.t, numeric.lower, numeric.upper, label = r"$\alpha$ = "+str(eq.alpha), alpha=.4)
+    #ax.plot(numeric.t, numeric.msd, label = r"$\alpha$ = "+str(eq.alpha))
     ax.plot(eq_.t, eq_.msd, label = "", color = "black", ls = ":")
     ax.set_xlim(xmin = eq.h)
     ax.legend()
     
 fig, ax = plt.subplots(1,3, figsize = (15,5))
-alpha = [0.05, 0.07, 0.09, 0.1]
+alpha = [0.05, 0.08, 0.4, 0.8]#, 0.3, 0.5, 0.7]
 
 avg = 4000
 task_set = ["001"]
 data_path = "_raw/time_crystal/"
 
 #Time crystal
-T = 100
-h = 0.05
+T = 15
+h = 0.005
 v0 = 1.0
 M = 1.0
 eta_1 = 0.0
@@ -48,11 +49,11 @@ for a in alpha:
             v0 = v0, M = M,
             eta_1 = eta_1, eta_2 = eta_2,
             T1 = T1, T2 = T2)
-    plot(axi, eq, avg, task_set, data_path, trunc = 20)
+    plot(axi, eq, avg, task_set, data_path, trunc = 15)
     
 #Time glass
-T = 100
-h = 0.05
+T = 15
+h = 0.005
 v0 = 1.0
 M = 1.0
 eta_1 = 1.0
@@ -68,11 +69,13 @@ for a in alpha:
             v0 = v0, M = M,
             eta_1 = eta_1, eta_2 = eta_2,
             T1 = T1, T2 = T2)
-    plot(axi, eq, avg, task_set, data_path, trunc = 20)
+    plot(axi, eq, avg, task_set, data_path, trunc = 15)
+
+alpha = [0.05, 0.08, 0.4, 0.8]
     
 #Mixed phase
-T = 100
-h = 0.05
+T = 15
+h = 0.005
 v0 = 1.0
 M = 1.0
 eta_1 = 1.0
@@ -88,7 +91,7 @@ for a in alpha:
             v0 = v0, M = M,
             eta_1 = eta_1, eta_2 = eta_2,
             T1 = T1, T2 = T2)
-    plot(axi, eq, avg, task_set, data_path, trunc = 20)
+    plot(axi, eq, avg, task_set, data_path, trunc = 15)
     
 fig.tight_layout()
 fig.savefig("outs/fig2_check.png", dpi = 200)
