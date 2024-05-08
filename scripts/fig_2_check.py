59d26c3ac0444c32ed6296402687357a34186457
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2024-05-08 19:39:34 +0200

    feat: split mean and CI options

--- scripts/fig_2_check.py
+++ scripts/fig_2_check.py
@@ -9,7 +9,7 @@ import plot as pt
 
 
 def plot(ax, eq, avg, task_set, data_path, trunc):
-    numeric = pt.numeric_msd(eq, avg, task_set, data_path)
+    numeric = pt.numeric_msd(eq, avg, task_set, data_path, bootstrap = True)
     numeric = numeric[numeric.t <= trunc]
     eq_ = fle(eq.alpha, eq.linear)
     eq_.params(T = trunc, h = 0.1,
@@ -18,8 +18,11 @@ def plot(ax, eq, avg, task_set, data_path, trunc):
             T1 = eq.T1, T2 = eq.T2)
     eq_.make_B_H()
     eq_.msd_non_linear()
-    ax.fill_between(numeric.t, numeric.lower, numeric.upper, label = r"$\alpha$ = "+str(eq.alpha), alpha=.4)
-    #ax.plot(numeric.t, numeric.msd, label = r"$\alpha$ = "+str(eq.alpha))
+    if all(np.array((numeric.lower == 0))) :
+        ax.plot(numeric.t, numeric.msd, label = r"$\alpha$ = "+str(eq.alpha))
+    else:
+        ax.fill_between(numeric.t, numeric.lower, numeric.upper, label = r"$\alpha$ = "+str(eq.alpha), alpha=.4)
+    
     ax.plot(eq_.t, eq_.msd, label = "", color = "black", ls = ":")
     ax.set_xlim(xmin = eq.h)
     ax.legend()
@@ -54,7 +57,7 @@ for a in alpha:
 #Time glass
 T = 15
 h = 0.005
-v0 = 1.0
+v0 = 0.0
 M = 1.0
 eta_1 = 1.0
 eta_2 = 0.0
