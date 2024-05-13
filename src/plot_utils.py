c964e4b1aaf6019c6c1d5ac6398a7e00c8c720e3
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2024-05-13 21:15:05 +0200

    refac: moved function to scale strings

--- src/plot_utils.py
+++ src/plot_utils.py
@@ -6,3 +6,9 @@ Created on Tue Feb 7 2024
 
 def add_caption_letter(ax, cap):
     ax.text(.02, .98, cap, ha='left', va='top', transform=ax.transAxes)
+    
+    
+def resize_names(ax, size = 10):
+    ax.xaxis.label.set_size(size)
+    ax.yaxis.label.set_size(size)   
+    ax.tick_params(axis = 'both', which = 'major', labelsize = size)
