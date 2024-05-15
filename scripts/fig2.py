e92196c3e2f65537adb32d684a85dcaaa43a9146
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2024-05-15 19:25:04 +0200

    refac: new layout plot

--- scripts/fig2.py
+++ scripts/fig2.py
@@ -11,17 +11,14 @@ from plot import *
 plt.style.use("analysis/plot_style.mplstyle")
 data_path = "_raw/time_crystal/"
 colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
-fig, ax = plt.subplots(1,3, figsize=(15, 5), sharex = True, sharey = True);
+fig, ax = plt.subplots(1,3, figsize=(15, 5), sharex = False, sharey = False);
 avg = 4000
-task_set = ["001", "002", "003", "004"]
+task_set = ["002", "003", "004", "005"]
+h_anl =0.1
 
-alpha = [0.05, 0.07, 0.09, 0.1]
+alpha = [0.01, 0.03, 0.05, 0.07, 0.09, 0.1]
 
-def resize_names(ax, size = 10):
-    ax.xaxis.label.set_size(size)
-    ax.yaxis.label.set_size(size)   
-    ax.tick_params(axis = 'both', which = 'major', labelsize = size)
-    
+   
 def add_axislabel_ins(axins, axi, x_text = "Freq. "+ r"$(1/t)$", y_text = "Amp."):
     props = dict(facecolor='white', edgecolor='white')
     axins.text(0.13, 0.65, x_text, transform=axi.transAxes, bbox=props, fontsize = 10)
@@ -31,11 +28,11 @@ def add_axislabel_ins(axins, axi, x_text = "Freq. "+ r"$(1/t)$", y_text = "Amp."
  ################ TIME CRYSTAL #################
 #################################################
 axi = ax[0]
-axins = axi.inset_axes([0.6, 0.08, 0.37, 0.37])
-axins_ = axi.inset_axes([0.1, 0.7, 0.27, 0.27])
+axins = axi.inset_axes([0.6, 1.17, 0.4, 0.4])
+axins_ = axi.inset_axes([0.0, 1.17, 0.4, 0.4])
 
-T = 100
-h = 0.05
+T = 15
+h = 0.005
 v0 = 1.0
 M = 1.0
 eta_1 = 0.0
@@ -44,60 +41,68 @@ T1 = 0.0
 T2 = 1.0
 
 linear = 0
-for i, a in enumerate(tqdm(alpha)):
+for i, a in enumerate(alpha):
+    print(a)
     eq = fle(a, linear)
     eq.params(T = T, h = h,
               v0 = v0, M = M,
               eta_1 = eta_1, eta_2 = eta_2,
               T1 = T1, T2 = T2)
+    if a == alpha[-1]:
+        legend_anl = True
+    else: 
+        legend_anl = False
     plot_msd(eq = eq, avg = avg, task_set = task_set, data_path = data_path, 
-             ax = axi, color_fd = colors[9 - i], legend_main = True)
+             ax = axi, color_fd = colors[9 - i], analytical = True, T = 15, h = h_anl, 
+             legend_main = True, legend_second = legend_anl, ci_normal = True)
     plot_msd(eq = eq, avg = avg, task_set = task_set, data_path = data_path, 
-             ax = axins, color_fd = colors[9 - i], legend_main = False, truncate = 25)
+             ax = axins, color_fd = colors[9 - i], analytical = False,
+             legend_main = False)
     
     df_ft = get_ft(eq, avg, task_set, data_path)
     plot_fft(axins_, df_ft, color = colors[9 - i], half = True)
     df_freq = get_freq(df_ft)
     
-add_1_npi(axins_, n = 1)
 add_1_npi(axins_, n = 2)
-resize_names(axins)
-resize_names(axins_)
-add_axislabel_ins(axins_, axi)
+pu.resize_names(axins)
+pu.resize_names(axins_)
 
-add_trend(axi, x0 = h, xf = 100, func = t, text = "~t", xtext = 0.1)
-add_trend(axi, x0 = h, xf = 1, func = t2, text = "~t²", xtext = 0.1)
-add_trend(axi, x0 = h, xf = 1, func = t3, text = "~t³", xtext = 0.1)
-add_perio_grid(axins, 1/np.pi, 25, times = 2)
+add_trend(axins, x0 = h, xf = 15, func = t, text = "~t", xtext = 0.01, fontsize = 11)
+add_trend(axins, x0 = h, xf = 1, func = t2, text = "~t²", xtext = 0.01, fontsize = 11)
+add_trend(axins, x0 = h, xf = 1, func = t3, text = "~t³", xtext = 0.01, fontsize = 11)
+add_perio_grid(axi, 1/np.pi, 15, times = 1)
 
-axi.set_xscale("log")
-axi.set_yscale("log")
+axins.set_xscale("log")
+axins.set_yscale("log")
 axins_.set_xscale("log")
 axins_.set_yscale("log")
 
 axi.set_ylabel("MSD " r"$\langle x^2 (t) \rangle$")
 axi.set_xlabel("Time "+"$t$")
-axi.set_xlim(xmin = h, xmax = 100)
-#axi.set_title("Time crystal")
-axins.set_xlabel("")
+axins.set_ylabel("MSD " r"$\langle x^2 (t) \rangle$")
+axins.set_xlabel("Time "+"$t$")
+axins_.set_ylabel("FFT - Amplitude ")
+axins_.set_xlabel("Freq. "+ r"$(1/t)$")
 
 
 handles, labels = axi.get_legend_handles_labels()
 axi.get_legend().remove()
 
 pu.add_caption_letter(axi, "(a)")
+pu.add_caption_letter(axins_, "(a-i)", fontsize = 12)
+pu.add_caption_letter(axins, "(a-ii)", fontsize = 12)
 
 #################################################
  ################ TIME GLASS #################
 #################################################
 
 axi = ax[1]
-axins = axi.inset_axes([0.6, 0.08, 0.37, 0.37])
-axins_ = axi.inset_axes([0.1, 0.7, 0.27, 0.27])
+axins = axi.inset_axes([0.6, 1.17, 0.4, 0.4])
+axins_ = axi.inset_axes([0.0, 1.17, 0.4, 0.4])
 
-T = 100
-h = 0.05
-v0 = 1.0
+T = 15
+h = 0.005
+v0 = 0.0
 M = 1.0
 eta_1 = 1.0
 eta_2 = 0.0
@@ -105,54 +110,67 @@ T1 = 1.0
 T2 = 0.0
 
 linear = 0
-for i, a in enumerate(tqdm(alpha)):
+for i, a in enumerate(alpha):
+    print(a)
     eq = fle(a, linear)
     eq.params(T = T, h = h,
               v0 = v0, M = M,
               eta_1 = eta_1, eta_2 = eta_2,
               T1 = T1, T2 = T2)
+    if a == alpha[-1]:
+        legend_anl = True
+    else: 
+        legend_anl = False
     plot_msd(eq = eq, avg = avg, task_set = task_set, data_path = data_path, 
-             ax = axi, color_fd = colors[9 - i])
+             ax = axi, color_fd = colors[9 - i], analytical = True, T = 15, h = h_anl, 
+             legend_main = True, legend_second = legend_anl, ci_normal = True)
     plot_msd(eq = eq, avg = avg, task_set = task_set, data_path = data_path, 
-             ax = axins, color_fd = colors[9 - i], legend_main = False, truncate = 25)
+             ax = axins, color_fd = colors[9 - i], analytical = False,
+             legend_main = False)
     
     df_ft = get_ft(eq, avg, task_set, data_path)
     plot_fft(axins_, df_ft, color = colors[9 - i], half = True)
     df_freq = get_freq(df_ft)
     
 add_1_npi(axins_, n = 1)
-resize_names(axins)
-resize_names(axins_)
-add_axislabel_ins(axins_, axi)
+pu.resize_names(axins)
+pu.resize_names(axins_)
 
-add_trend(axi, x0 = h, xf = 100, func = t, text = "~t", xtext = 0.1)
-add_trend(axi, x0 = h, xf = 1, func = t2, text = "~t²", xtext = 0.1)
-add_trend(axi, x0 = h, xf = 1, func = t3, text = "~t³", xtext = 0.1)
-add_perio_grid(axins, 1/np.pi, 25, times = 2)
+add_trend(axins, x0 = h, xf = 15, func = t, text = "~t", xtext = 0.01, fontsize = 11)
+add_trend(axins, x0 = h, xf = 1, func = t2, text = "~t²", xtext = 0.01, fontsize = 11)
+add_trend(axins, x0 = h, xf = 1, func = t3, text = "~t³", xtext = 0.01, fontsize = 11)
+add_perio_grid(axi, 1/np.pi, 15, times = 1)
 
-axi.set_xscale("log")
-axi.set_yscale("log")
+axins.set_xscale("log")
+axins.set_yscale("log")
 axins_.set_xscale("log")
 axins_.set_yscale("log")
 
 axi.set_ylabel("MSD " r"$\langle x^2 (t) \rangle$")
 axi.set_xlabel("Time "+"$t$")
-axi.set_xlim(xmin = h, xmax = 100)
-#axi.set_title("Time glass")
-axins.set_xlabel("")
+axins.set_ylabel("MSD " r"$\langle x^2 (t) \rangle$")
+axins.set_xlabel("Time "+"$t$")
+axins_.set_ylabel("FFT - Amplitude ")
+axins_.set_xlabel("Freq. "+ r"$(1/t)$")
+
+
+handles, labels = axi.get_legend_handles_labels()
+axi.get_legend().remove()
 
 pu.add_caption_letter(axi, "(b)")
+pu.add_caption_letter(axins_, "(b-i)", fontsize = 12)
+pu.add_caption_letter(axins, "(b-ii)", fontsize = 12)
 
 #################################################
  ################ MIXED PHASE #################
 #################################################
 
 axi = ax[2]
-axins = axi.inset_axes([0.6, 0.08, 0.37, 0.37])
-axins_ = axi.inset_axes([0.1, 0.7, 0.27, 0.27])
+axins = axi.inset_axes([0.6, 1.17, 0.4, 0.4])
+axins_ = axi.inset_axes([0.0, 1.17, 0.4, 0.4])
 
-T = 100
-h = 0.05
+T = 15
+h = 0.005
 v0 = 1.0
 M = 1.0
 eta_1 = 1.0
@@ -161,51 +179,61 @@ T1 = 1.0
 T2 = 1.0
 
 linear = 0
-for i, a in enumerate(tqdm(alpha)):
+for i, a in enumerate(alpha):
+    print(a)
     eq = fle(a, linear)
     eq.params(T = T, h = h,
               v0 = v0, M = M,
               eta_1 = eta_1, eta_2 = eta_2,
               T1 = T1, T2 = T2)
+    if a == alpha[-1]:
+        legend_anl = True
+    else: 
+        legend_anl = False
     plot_msd(eq = eq, avg = avg, task_set = task_set, data_path = data_path, 
-             ax = axi, color_fd = colors[9 - i])
+             ax = axi, color_fd = colors[9 - i], analytical = True, T = 15, h = h_anl, 
+             legend_main = True, legend_second = legend_anl, ci_normal = True)
     plot_msd(eq = eq, avg = avg, task_set = task_set, data_path = data_path, 
-             ax = axins, color_fd = colors[9 - i], legend_main = False, truncate = 25)
+             ax = axins, color_fd = colors[9 - i], analytical = False,
+             legend_main = False)
     
     df_ft = get_ft(eq, avg, task_set, data_path)
     plot_fft(axins_, df_ft, color = colors[9 - i], half = True)
     df_freq = get_freq(df_ft)
     
 add_1_npi(axins_, n = 3)
-#add_1_npi(axins_, n = 2)
-resize_names(axins)
-resize_names(axins_)
-add_axislabel_ins(axins_, axi)
+pu.resize_names(axins)
+pu.resize_names(axins_)
 
-add_trend(axi, x0 = h, xf = 100, func = t, text = "~t", xtext = 0.1)
-add_trend(axi, x0 = h, xf = 1, func = t2, text = "~t²", xtext = 0.1)
-add_trend(axi, x0 = h, xf = 1, func = t3, text = "~t³", xtext = 0.1)
-add_perio_grid(axins, 1/np.pi, 25, times = 2)
+add_trend(axins, x0 = h, xf = 15, func = t, text = "~t", xtext = 0.01, fontsize = 11)
+add_trend(axins, x0 = h, xf = 1, func = t2, text = "~t²", xtext = 0.01, fontsize = 11)
+add_trend(axins, x0 = h, xf = 1, func = t3, text = "~t³", xtext = 0.01, fontsize = 11)
+add_perio_grid(axi, 1/np.pi, 15, times = 1)
 
-axi.set_xscale("log")
-axi.set_yscale("log")
+axins.set_xscale("log")
+axins.set_yscale("log")
 axins_.set_xscale("log")
 axins_.set_yscale("log")
 
 axi.set_ylabel("MSD " r"$\langle x^2 (t) \rangle$")
 axi.set_xlabel("Time "+"$t$")
-axi.set_xlim(xmin = h, xmax = 100)
-#axi.set_title("Time crystal + time glass")
-axins.set_xlabel("")
+axins.set_ylabel("MSD " r"$\langle x^2 (t) \rangle$")
+axins.set_xlabel("Time "+"$t$")
+axins_.set_ylabel("FFT - Amplitude ")
+axins_.set_xlabel("Freq. "+ r"$(1/t)$")
+
+
+handles, labels = axi.get_legend_handles_labels()
+axi.get_legend().remove()
 
 pu.add_caption_letter(axi, "(c)")
+pu.add_caption_letter(axins_, "(c-i)", fontsize = 12)
+pu.add_caption_letter(axins, "(c-ii)", fontsize = 12)
 
 ###### ###### ######
  ###### SAVE ######
 ###### ###### ######
-fig.legend(handles, labels, bbox_to_anchor = (0.95, 1.1), ncol = 8)
+fig.legend(handles, labels, bbox_to_anchor = (0.95, 1.2), ncol = 8)
 fig.tight_layout()
 
-fig.savefig("outs/fig2_fix.png", dpi = 200)
-#fig.savefig("outs/fig2_500dpi_h001.png", dpi = 500)
-#fig.savefig("outs/fig2_1000dpi_h001.png", dpi = 1000)
+fig.savefig("outs/fig2_.png", dpi = 100)
